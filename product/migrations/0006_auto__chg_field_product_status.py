3228f30d5c9f0b8ad773b3c57e92dc4c4feeb9e8
Łukasz Lechowicz <llechowicz@gmail.com>
2013-03-29 20:16:50 +0000

    Product.status type changed from char to int

---
+++ product/migrations/0006_auto__chg_field_product_status.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+
+        # Changing field 'Product.status'
+        db.alter_column(u'product_product', 'status', self.gf('django.db.models.fields.IntegerField')())
+
+    def backwards(self, orm):
+
+        # Changing field 'Product.status'
+        db.alter_column(u'product_product', 'status', self.gf('django.db.models.fields.CharField')(max_length=64))
+
+    models = {
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'cs_user.company': {
+            'Meta': {'object_name': 'Company'},
+            'created': ('django.db.models.fields.DateTimeField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'logo': ('django.db.models.fields.files.FileField', [], {'max_length': '100', 'blank': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '128'})
+        },
+        u'cs_user.user': {
+            'Meta': {'object_name': 'User'},
+            'address': ('django.db.models.fields.CharField', [], {'max_length': '128', 'blank': 'True'}),
+            'city': ('django.db.models.fields.CharField', [], {'max_length': '128', 'blank': 'True'}),
+            'company': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['cs_user.Company']", 'null': 'True'}),
+            'company_name': ('django.db.models.fields.CharField', [], {'max_length': '128', 'blank': 'True'}),
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'postcode': ('django.db.models.fields.CharField', [], {'max_length': '8', 'blank': 'True'}),
+            'primary_phone': ('django.db.models.fields.CharField', [], {'max_length': '9'}),
+            'role': ('django.db.models.fields.IntegerField', [], {'default': '30'}),
+            'secondary_phone': ('django.db.models.fields.CharField', [], {'max_length': '9', 'blank': 'True'}),
+            'subscriber': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        u'product.comment': {
+            'Meta': {'ordering': "['created']", 'object_name': 'Comment'},
+            'cost_hardware': ('django.db.models.fields.FloatField', [], {'default': '0.0'}),
+            'cost_service': ('django.db.models.fields.FloatField', [], {'default': '0.0'}),
+            'cost_transport': ('django.db.models.fields.FloatField', [], {'default': '0.0'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {}),
+            'description': ('django.db.models.fields.TextField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {}),
+            'product': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['product.Product']"}),
+            'status': ('django.db.models.fields.IntegerField', [], {}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['cs_user.User']"})
+        },
+        u'product.courier': {
+            'Meta': {'object_name': 'Courier'},
+            'created': ('django.db.models.fields.DateTimeField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': "'64'"})
+        },
+        u'product.product': {
+            'Meta': {'ordering': "['-modified']", 'object_name': 'Product'},
+            'company': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['cs_user.Company']"}),
+            'created': ('django.db.models.fields.DateTimeField', [], {}),
+            'description': ('django.db.models.fields.TextField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'invoice': ('django.db.models.fields.CharField', [], {'max_length': '128', 'blank': 'True'}),
+            'key': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'max_cost': ('django.db.models.fields.FloatField', [], {'default': '0.0'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'producent': ('django.db.models.fields.CharField', [], {'max_length': '128', 'blank': 'True'}),
+            'serial': ('django.db.models.fields.CharField', [], {'max_length': '128', 'blank': 'True'}),
+            'status': ('django.db.models.fields.IntegerField', [], {}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['cs_user.User']"}),
+            'warranty': ('django.db.models.fields.CharField', [], {'default': "'nie'", 'max_length': '8'})
+        }
+    }
+
+    complete_apps = ['product']
