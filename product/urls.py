f0fd8a5c3289b2bbfe228b2dc5ad692b7e3e8211
Łukasz Lechowicz <llechowicz@gmail.com>
2013-03-30 12:39:38 +0000

    filter css and url tweaks

--- product/urls.py
+++ product/urls.py
@@ -5,7 +5,7 @@ urlpatterns = patterns('product.views',
 
     # products
     url(r'^lista$', 'product_list', name='product_list'),
-    url(r'^lista/(?P<status>\d+)$', 'product_list',  name='product_list'),
+    url(r'^lista/status/(?P<status>\d+)$', 'product_list',  name='product_list'),
     url(r'^klient/(?P<user_pk>\d+)/zgloszenie/(?P<pk>\d+)/szczegoly$', 'product_detail', name='product_detail'),
     url(r'^klient/(?P<user_pk>\d+)/zglozsenie/(?P<pk>\d+)/pdf$', 'product_pdf', name='product_pdf'),
     url(r'^klient/(?P<user_pk>\d+)/zglozsenie/nowe$', 'product_create', name='product_create'),
