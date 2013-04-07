dc472f3c1c91a420a43c1f7356abe9d7404ebb7f
Łukasz Lechowicz <llechowicz@gmail.com>
2013-04-07 20:30:48 +0100

    added dziobak to allowed hosts

--- cs/settings.py
+++ cs/settings.py
@@ -31,7 +31,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'localhost:8000', '54.228.212.183']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'localhost:8000', '54.228.212.183', 'dziobak.lechkom.pl']
 
 TIME_ZONE = 'Poland'
 LANGUAGE_CODE = 'pl'
